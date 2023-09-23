d6c3036ca2c5bd82cb62172dc43341cb076d9a35
moura1001 <gmouraneto01@gmail.com>
2023-09-23 10:41:19 -0300

    commit com configuração inicial do projeto

---
+++ src/main/java/com/moura/sistemapagamentosbackend/SistemaPagamentosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moura.sistemapagamentosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPagamentosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPagamentosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moura/sistemapagamentosbackend/SistemaPagamentosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura.sistemapagamentosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagamentosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
